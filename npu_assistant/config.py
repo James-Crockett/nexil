800ad6d8c5d896edb83a54dd5147657d07f8533d
James-Crockett <nitheesh.gnk777@gmail.com>
2026-03-03 12:58:53 -0500

    core: working on config file

--- npu_assistant/config.py
+++ npu_assistant/config.py
@@ -1,10 +1,21 @@
-[config]
-    name = "Assistant"
-    system_prompt = ""
-    thinking = False
-    device = "NPU"
-    model_path = "~/.cache/npu-assistant/models/Qwen2.5-3B-Instruct-int4-ov"
-    do_sample = False
-    max_new_tokens = 512
-    repetition_penalty = 1.1
+#imports
+from pathlib import Path
+from dataclasses import dataclass
+
+#default settings
+CONFIG_PATH = Path.home() / ".config" / "npu-assistant" / "config.toml"
+DEFAULT_MODEL_PATH = Path.home() / ".config" / "npu-assistant" / "models" / "Qwen2.5-3B-Instruct-int4-ov"
+
+
+@dataclass 
+class config:
+    name: str = "Assistant"
+    system_prompt: str = DEFAULT_PROMPT
+    device: str = "NPU"
+    model_path: str = DEFAULT_MODEL_PATH
+    do_sample: bool = False
+    max_new_tokens: int = 512
+    rep_penalty: float = 1.1
+
     
+        
