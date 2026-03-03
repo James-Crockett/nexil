22ba87debd543aa6bd1887afa49ccddeeb45efb7
James-Crockett <nitheesh.gnk777@gmail.com>
2026-03-03 13:42:03 -0500

    core: listing devices onboard

---
+++ npu_assistant/devices.py
@@ -0,0 +1,22 @@
+import openvino as ov
+from rich.console import Console
+from rich.table import Table
+
+
+def cmd_devices():
+
+    #get devices
+    core = ov.Core()
+    devices = core.available_devices
+
+    table = Table()
+    table.add_column("Devices Onboard", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Device Name", justify="right", style="cyan", no_wrap=True)
+    for device in devices:
+        device_name = core.get_property(device, "FULL_DEVICE_NAME")
+        table.add_row(device,device_name)
+      
+    console = Console()
+    console.print(table) 
+
+cmd_devices()
